2de794db5d1c51af51899723b8b2aa51c24290bf
hingyi khong and Krystal Foster <hingyi@>
2018-08-27 12:04:42 -0700

    delete extra files

--- img/Creative Pep Talk _ Free Listening on SoundCloud_files/rules-p-v6vhDK1Eg0ZDD.js
+++
@@ -1,7 +0,0 @@
-/*
- Quantcast measurement tag
- Copyright (c) 2008-2017, Quantcast Corp.
-*/
-(function(c,h,g){try{__qc("defaults",c,{labels:"_fp.event.Default"})}catch(m){}var k=function(a,b,e){return a?"nc"===a?!b||!e||0>b.indexOf(e):"eq"===a?b===e:"sw"===a?0===b.indexOf(e):"ew"===a?(a=b.length-e.length,b=b.lastIndexOf(e,a),-1!==b&&b===a):"c"===a?0<=b.indexOf(e):!1:!1},l=function(a){var b=g.createElement("a");b.href=a;return b},f=function(a,b,e){var c;c=h.top===h.self?g.location.href:l(g.referrer).href;k(b,c,e)?a(c):a(!1)},d=function(a){return"array"==={}.toString.call(a).match(/\s([a-zA-Z]+)/)[1].toLowerCase()?
-{labels:a.join(",")}:{labels:""+a}};__qc("rules",[c,null,[[d,"_fp.event.SoundCloud Pro Landing Page"]],[[f,"eq","https://soundcloud.com/pro/buy/pro"]]],[c,null,[[d,"_fp.event.SoundCould Pro Unlimited Landing Page"]],[[f,"eq","https://soundcloud.com/pro/buy/pro-unlimited"]]],[c,null,[[d,"_fp.event.Welcome Page"]],[[f,"eq","https://soundcloud.com/welcome"]]],[c,null,[[d,"_fp.event.Go Page"]],[[f,"eq","https://soundcloud.com/go"]]],[c,null,[[d,"_fp.event.Get SoundCloud Go"]],[[f,"eq","https://soundcloud.com/go/buy/go"]]],
-[c,null,[[d,"_fp.event.Upload Page"]],[[f,"c","upload"]]],[c,null,[[d,"_fp.event.Upload Page"]],[[f,"eq","https://soundcloud.com/upload"]]],[c,null,[[d,"_fp.event.Homepage"]],[[function(a,b,c){var d;d=h.top===h.self?g.location.pathname:l(g.referrer).pathname;k(b,d,c)?a(d):a(!1)},"eq","/"]]])})("p-v6vhDK1Eg0ZDD",window,document);
