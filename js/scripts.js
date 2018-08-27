ab3b2793db4887f9ae6399269159a923054fb0f3
hingyi khong and Krystal Foster <krystal@>
2018-08-27 16:50:46 -0700

    resolve conflicts

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+var creativePepTalk = new Podcast ('name', 'image', 'text', 'embed', 'link', 'intelligence', 'truthiness')
+var creativePepTalk = new Podcast ('name', 'image', 'text', 'embed', 'link', 'intelligence', 'truthiness')
+
+
+var podCastList = [creativePepTalk, Lore];
+
+function Modal(name, image, text, embed, link) {
+  this.name = name;
+  this.image = image;
+  this.text = text;
+  this.embed = embed;
+  this.link = link;
+}
+
+Modal.prototype.makeModal = function(podType) {
+  if (podType === "intelligence")
+
+  //function to show specifc modal content
+}
+
+
+function suggestion(podcast) {
+  podCastList.forEach(function(podcast) {
+    if (podcast.truthiness === "true") {
+
+    }
+  })
+
+}
+
+
+
+var newPodcast(name, image, text, )
