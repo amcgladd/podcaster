6c77227e6a5ade66e90d15de4cc05a6e5154e864
Student <example@example.com>
2018-08-28 10:28:56 -0700

    add scoring logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,67 @@
-switch (score) {
-  case "1,1":
+// switch (score) {
+//   case "1,1":
+//
+//     break;
+//   case "1,2":
+//
+//     break;
+//   case "1,3":
+//
+//     break;
+//   case "1,4":
+//
+//   break;
+//   case "2,1":
+//
+//     break;
+//   case "2,2":
+//
+//     break;
+//   case "2,3":
+//
+//     break;
+//   case "2,4":
+//
+//     break;
+//   case "3,1":
+//
+//     break;
+//   case "3,2":
+//
+//     break;
+//   case "3,3":
+//
+//     break;
+//   case "3,4":
+//
+//   break;
+//   case "4,1":
+//
+//     break;
+//   case "4,2":
+//
+//     break;
+//   case "4,3":
+//
+//     break;
+//   case "4,4":
+//
+//     break;
+// }
 
-    break;
-  case "1,2":
 
-    break;
-  case "1,3":
 
-    break;
-  case "1,4":
 
-  break;
-  case "2,1":
-
-    break;
-  case "2,2":
-
-    break;
-  case "2,3":
-
-    break;
-  case "2,4":
-
-    break;
-  case "3,1":
-
-    break;
-  case "3,2":
-
-    break;
-  case "3,3":
-
-    break;
-  case "3,4":
-
-  break;
-  case "4,1":
-
-    break;
-  case "4,2":
-
-    break;
-  case "4,3":
-
-    break;
-  case "4,4":
-
-    break;
+finalScoreProducer (truth,smarts) {
+  var truthScore = 0;
+  var intelligence = 0;
+  if (truth >= 0 && truth <= 5) {
+    truthScore = 1;
+  }
 }
 
 
-// var score = function(inputArray) {
-//   var truthiness = 0;
-//   var intelligence = 0;
-//
-// inputArray.map(function(type))
-//
-// }
-//
-//
+
 
 //front-end logic
 $(document).ready(function() {
@@ -77,6 +80,65 @@ $(document).ready(function() {
   });
   var answer8 = $("input:radio[name=question8]:checked").val();
 
+  //scoring logic
+  var truthiness = 0;
+  var intelligence = 0;
+
+  //q1 logic
+  if (answer1 === "1A") {
+    truthiness += 5;
+  }
+
+  //q2 logic
+  if (answer2 === "2A") {
+    intelligence += 5;
+  } else if (answer2 === "2C") {
+    intelligence += 2;
+  }
+
+  //q3 logic
+  truthiness += answer3;
+
+  //q4 logic
+  if (answer4 === "4B") {
+    truthiness += 3;
+  } else if (answer4 === "4C") {
+    truthiness += 5;
+  }
+
+  //q5 logic
+  if (answer5 === "5B") {
+    intelligence += 1;
+  } else if (answer5 === "5C") {
+    intelligence +=3;
+  } else if (answer5 === "5D") {
+    intelligence +=5;
+  }
+
+  //q6 logic
+  if (answer6 === "6B") {
+    truthiness += 5;
+  } else if (answer6 === "6C") {
+    truthiness +=3;
+  } else if (answer6 === "6D") {
+    truthiness +=1;
+  }
+
+  //q7 logic
+  intelligence += answer7Array.length;
+
+  //q8 logic
+  if (answer8 === "8B") {
+    intelligence +=1;
+  } else if (answer8 === "8C") {
+    intelligence +=3;
+  } else if (answer8 === "8D") {
+    intelligence +=5;
+  }
+
+finalScoreProducer (truthiness,intelligence);
+
+  console.log("truthiness ="+ truthiness, "intelligence ="+ intelligence);
   console.log("answer1 = "+answer1,"answer2 = "+ answer2,"answer3 = "+answer3,"answer4 = "+answer4,"answer5 = "+answer5,"answer6 = "+answer6,"answer7 = "+answer7Array,"answer8 = "+answer8);
   })
 });
