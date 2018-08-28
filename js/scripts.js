1012350524337dbeff07d47f0ef60ed8a2dcab5d
Student <example@example.com>
2018-08-28 11:08:54 -0700

    links scores to cases

--- js/scripts.js
+++ js/scripts.js
@@ -1,103 +1,158 @@
-var finalScore = truthiness + ',' + intelligence;
+function finalScoreProducer (truth,smarts) {
+  console.log(truth,smarts);
+  var truthScore = 0;
+  var intelligenceScore = 0;
+  if (truth >= 0 && truth <= 5) {
+    truthScore = 1;
+  } else if (truth >= 6 && truth <= 10) {
+    truthScore = 2;
+  } else if (truth >= 11 && truth <= 15) {
+    truthScore = 3;
+  } else if (truth >= 16) {
+    truthScore = 4;
+  }
 
-
-switch (score) {
-  case "1,1":
-  $("#comedyBangBang").modal('show')
-
-    break;
-  case "1,2":
-  $("#noSleep").modal('show')
-
-    break;
-  case "1,3":
-  $("#burtonReads").modal('show')
-    break;
-  case "1,4":
-  $("#bingeMode").modal('show')
-
-  break;
-  case "2,1":
-  $("#casketDelights").modal('show')
-
-    break;
-  case "2,2":
-  $("#nightVale").modal('show')
-
-    break;
-  case "2,3":
-  $("#lore").modal('show')
-
-    break;
-  case "2,4":
-  $("#ridiculousHistory").modal('show')
-    break;
-  case "3,1":
-  $("#creativePepTalk").modal('show')
-
-    break;
-  case "3,2":
-  $("#creativePepTalk").modal('show')
-
-    break;
-  case "3,3":
-  $("#rossAndCarrie").modal('show')
-
-    break;
-  case "3,4":
-  $("#theModernAcre").modal('show')
-
-  break;
-  case "4,1":
-  $("#waitWait").modal('show')
-
-    break;
-  case "4,2":
-  $("#noSuchFish").modal('show')
-
-    break;
-  case "4,3":
-  $("#smallDoses").modal('show')
-
-    break;
-  case "4,4":
-  $("#bigThink").modal('show')
-
-    break;
+  if (smarts >= 0 && smarts <= 5) {
+    intelligenceScore = 1;
+  } else if (smarts >= 6 && smarts <= 10) {
+    intelligenceScore = 2;
+  } else if (smarts >= 11 && smarts <= 15) {
+    intelligenceScore = 3;
+  } else if (smarts >= 16) {
+    intelligenceScore = 4;
+  }
+  return truthScore +","+ intelligenceScore;
 }
+  //front-end logic
+  $(document).ready(function() {
+    $("#user-input").submit(function(event){
+      event.preventDefault();
 
+      var answer1 = $("input:radio[name=question1]:checked").val();
+      var answer2 = $("input:radio[name=question2]:checked").val();
+      var answer3 = $("#question3").val();
+      var answer4 = $("#question4").val();
+      var answer5 = $("input:radio[name=question5]:checked").val();
+      var answer6 = $("#question6").val();
+      var answer7Array = [];
+      var answer7 = $("input:checkbox:checked").each(function() {
+        answer7Array.push($(this).val());
+      });
+      var answer8 = $("input:radio[name=question8]:checked").val();
 
-var score = function(inputArray) {
-  var truthiness = 0;
-  var intelligence = 0;
+      //scoring logic
+        var truthiness = 0;
+        var intelligence = 0;
 
-inputArray.map(function(type))
+        //q1 logic
+        if (answer1 === "1A") {
+          truthiness += 5;
+        }
 
-}
+        //q2 logic
+        if (answer2 === "2A") {
+          intelligence += 5;
+        } else if (answer2 === "2C") {
+          intelligence += 2;
+        }
 
+        //q3 logic
+        truthiness += answer3;
 
+        //q4 logic
+        if (answer4 === "4B") {
+          truthiness += 3;
+        } else if (answer4 === "4C") {
+          truthiness += 5;
+        }
 
+        //q5 logic
+        if (answer5 === "5B") {
+          intelligence += 1;
+        } else if (answer5 === "5C") {
+          intelligence +=3;
+        } else if (answer5 === "5D") {
+          intelligence +=5;
+        }
 
+        //q6 logic
+        if (answer6 === "6B") {
+          truthiness += 5;
+        } else if (answer6 === "6C") {
+          truthiness +=3;
+        } else if (answer6 === "6D") {
+          truthiness +=1;
+        }
 
-//front-end logic
-$(document).ready(function() {
-  $("#user-input").submit(function(event){
-    event.preventDefault();
+        //q7 logic
+        intelligence += answer7Array.length;
 
+        //q8 logic
+        if (answer8 === "8B") {
+          intelligence +=1;
+        } else if (answer8 === "8C") {
+          intelligence +=3;
+        } else if (answer8 === "8D") {
+          intelligence +=5;
+        }
 
-  var answer1 = $("input:radio[name=question1]:checked").val();
-  var answer2 = $("input:radio[name=question2]:checked").val();
-  var answer3 = $("#question3").val();
-  var answer4 = $("#question4").val();
-  var answer5 = $("input:radio[name=question5]:checked").val();
-  var answer6 = $("#question6").val();
-  var answer7Array = [];
-  var answer7 = $("input:checkbox:checked").each(function() {
-    answer7Array.push($(this).val());
+      var finalScore = finalScoreProducer(truthiness,intelligence);
+      console.log(finalScore);
+
+      var truthiness = 0;
+      var intelligence = 0;
+
+      switch (finalScore) {
+        case "1,1":
+        $("#comedyBangBang").modal('show');
+        break;
+        case "1,2":
+        $("#noSleep").modal('show');
+        break;
+        case "1,3":
+        $("#burtonReads").modal('show');
+        break;
+        case "1,4":
+        $("#bingeMode").modal('show');
+        break;
+        case "2,1":
+        $("#casketDelights").modal('show');
+        break;
+        case "2,2":
+        $("#nightVale").modal('show');
+        break;
+        case "2,3":
+        $("#lore").modal('show');
+        break;
+        case "2,4":
+        $("#ridiculousHistory").modal('show');
+        break;
+        case "3,1":
+        $("#creativePepTalk").modal('show');
+        break;
+        case "3,2":
+        $("#creativePepTalk").modal('show');
+        break;
+        case "3,3":
+        $("#rossAndCarrie").modal('show');
+        break;
+        case "3,4":
+        $("#theModernAcre").modal('show');
+        break;
+        case "4,1":
+        $("#waitWait").modal('show');
+        break;
+        case "4,2":
+        $("#noSuchFish").modal('show');
+        break;
+        case "4,3":
+        $("#smallDoses").modal('show');
+        break;
+        case "4,4":
+        $("#bigThink").modal('show');
+        break;
+        default: alert("whoops! something went wrong!")
+      }
+      console.log("answer1 = "+answer1,"answer2 = "+ answer2,"answer3 = "+answer3,"answer4 = "+answer4,"answer5 = "+answer5,"answer6 = "+answer6,"answer7 = "+answer7Array,"answer8 = "+answer8);
+    });
   });
-  var answer8 = $("input:radio[name=question8]:checked").val();
-
-
-  console.log("answer1 = "+answer1,"answer2 = "+ answer2,"answer3 = "+answer3,"answer4 = "+answer4,"answer5 = "+answer5,"answer6 = "+answer6,"answer7 = "+answer7Array,"answer8 = "+answer8);
-  })
-});
